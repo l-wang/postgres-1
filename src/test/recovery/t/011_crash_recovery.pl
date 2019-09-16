ce99fc13429807e97118e40d0d3f6a2e3430d7fd
Alexandra Wang <lewang@pivotal.io>
2019-09-16 13:40:19 -0700

    Add test for standby replay after reached consistency

--- src/test/recovery/t/011_crash_recovery.pl
+++ src/test/recovery/t/011_crash_recovery.pl
@@ -7,6 +7,7 @@ use PostgresNode;
 use TestLib;
 use Test::More;
 use Config;
+use File::Path qw(rmtree);
 if ($Config{osname} eq 'MSWin32')
 {
 
@@ -15,7 +16,7 @@ if ($Config{osname} eq 'MSWin32')
 }
 else
 {
-	plan tests => 5;
+	plan tests => 6;
 }
 
 my $node = get_new_node('master');
@@ -114,6 +115,53 @@ $node_standby->stop('immediate');
 # Should restart ignoring directory creation error.
 is($node_standby->start(fail_ok => 1), 1);
 
+# Ensure that a missing tablespace directory immediately causes panic if standby
+# has already reached recovery consistency.
+
+$node_master = get_new_node('master4');
+$node_master->init(allows_streaming => 1);
+$node_master->start;
+
+# Create tablespace
+$tspDir_master = TestLib::tempdir;
+$realTSDir_master = TestLib::perl2host($tspDir_master);
+$node_master->safe_psql('postgres', "CREATE TABLESPACE ts1 LOCATION '$realTSDir_master'");
+
+$tspDir_standby = TestLib::tempdir;
+$realTSDir_standby = TestLib::perl2host($tspDir_standby);
+
+# Take backup
+$backup_name = 'my_backup';
+$node_master->backup($backup_name,
+					 tablespace_mappings =>
+					   "$realTSDir_master=$realTSDir_standby");
+$node_standby = get_new_node('standby4');
+$node_standby->init_from_backup($node_master, $backup_name, has_streaming => 1);
+$node_standby->start;
+
+# Make sure standby reached consistency and starts accepting connections
+$node_standby->poll_query_until('postgres', 'SELECT 1', '1');
+
+# Pause replication
+$node_standby->safe_psql('postgres', 'SELECT pg_wal_replay_pause()');
+
+# Remove standby tablespace directory so it will be missing when replay resumes
+rmtree($realTSDir_standby);
+
+# Create a database in the tablespace and a table in default tablespace
+$node_master->safe_psql('postgres',
+						q[CREATE TABLE should_not_replay_insertion(a int);
+						  CREATE DATABASE db1 WITH TABLESPACE ts1;
+						  INSERT INTO should_not_replay_insertion VALUES (1);]);
+
+# Resume replication on standby and make sure it's caught up
+$node_standby->safe_psql('postgres', 'SELECT pg_wal_replay_resume()');
+$node_master->wait_for_catchup($node_standby, 'replay',
+							   $node_master->lsn('replay'));
+
+# Standby should panic and should not silently skip replaying the wal
+is($node_standby->safe_psql('postgres', 'SELECT count(*) = 0 FROM should_not_replay_insertion'),
+	't', 'insertion should not be replayed');
 
 # Ensure that tablespace removal doesn't cause error while recovering the
 # preceding alter database set tablespace.
